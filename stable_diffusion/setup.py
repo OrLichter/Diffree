cd07fd9526295b26df5e4244960f4c328c294031
OrLichter <orlichter@gmail.com>
2025-01-21 13:26:57 +0200

    Update setup.py

--- stable_diffusion/setup.py
+++ stable_diffusion/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='latent-diffusion',
+    name='diffree_stable_diffusion',
     version='0.0.1',
     description='',
     packages=find_packages(),
