7c917f215d398ff79df1639a77666de690d10fc4
Or Lichter <or_lichter@apple.com>
2025-01-21 13:35:14 +0200

    move setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='diffree_stable_diffusion',
+    name='stable_diffusion',
     version='0.0.1',
     description='',
     packages=find_packages(),
